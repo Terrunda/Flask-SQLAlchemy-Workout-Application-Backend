eb42c3a6d9e6a34fc9fcb2fa2565d22034d876a6
Terrunda <terrencegift49@gmail.com>
2026-04-20 18:27:36 +0300

    Created the tables with the correspondign attributes for each of the tables.

--- server/models.py
+++ server/models.py
@@ -1,5 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
-db = SQLAlchemy()
+from sqlalchemy import MetaData
+
+
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
 
 # Define Models here
+# Exercise table
+class Exercise(db.Model):
+    __tablename__ = 'exercise'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean)
+
+
+# Workout table
+class Workout(db.Model):
+    __tablename__ = 'workout'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    duration = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+
+# Workout exercises table (junction table for many to many relationship)
+class WorkoutExercises(db.Model):
+    __tablename__ = 'workoutexercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'))
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'))
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
